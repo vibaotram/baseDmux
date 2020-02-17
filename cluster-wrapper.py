d564e2b0421830889ad17a4a4cb5870d91502674
Tram <vbt576@gmail.com>
2020-02-17 17:42:04 +0700

    update 'sbatch'

--- cluster-wrapper.py
+++ cluster-wrapper.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import sys
 from snakemake.utils import read_job_properties
 
 
@@ -8,12 +9,16 @@ jobscript = sys.argv[-1]
 
 job_properties = read_job_properties(jobscript)
 
-
-job_properties["cluster"]["time"]
+rule = job_properties["rule"]
 jobname = job_properties["cluster"]["jobname"]
 partition = job_properties["cluster"]["partition"]
 cores = job_properties["cluster"]["cores"]
 account = job_properties["cluster"]["account"]
+output = job_properties["cluster"]["output"]
+error = job_properties["cluster"]["error"]
 
-cmdline = 'sbatch -j {jobname} -p {partition} -A {account} -c {cores} {script}" '.format(jobname=jobname, partition=partition, account=account, cores=cores, script=jobscript)
+if rule == "guppy_basecalling":
+    cmdline = 'sbatch -J {jobname} -p {partition} -A {account} -c {cores} {script}'.format(jobname=jobname, partition=partition, account=account, cores=cores, script=jobscript)
+else:
+    cmdline = 'sbatch -J {jobname} -p {partition} {script}'.format(jobname=jobname, partition=partition, script=jobscript)
 os.system(cmdline)
