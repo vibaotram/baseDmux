24cd6615252942678e5a29a05b59f2e9b2d9a936
Tram <vbt576@gmail.com>
2020-03-23 12:41:57 +0700

    get rid of cluster.json

--- cluster-wrapper.py
+++ cluster-wrapper.py
@@ -1,22 +1,48 @@
 #!/usr/bin/env python3
+
+'''
+wrap ...
+'''
 import os
 import sys
 from snakemake.utils import read_job_properties
-
+from snakemake import load_configfile
 
 jobscript = sys.argv[-1]
-
+config = 'config.yaml'
 
 job_properties = read_job_properties(jobscript)
+config_properties = load_configfile(config)
 
-rule = job_properties["rule"]
-jobname = job_properties["cluster"]["jobname"]
-partition = job_properties["cluster"]["partition"]
-cores = job_properties["cluster"]["cores"]
-account = job_properties["cluster"]["account"]
-output = job_properties["cluster"]["output"]
-error = job_properties["cluster"]["error"]
 
-cmdline = 'sbatch -J {jobname} -p {partition} -A {account} -c {cores} {script}'.format(jobname=jobname, partition=partition, account=account, cores=cores, script=jobscript)
+
+# rule = job_properties["rule"]
+# jobname = job_properties["cluster"]["jobname"]
+# partition = job_properties["cluster"]["partition"]
+# cores = job_properties["cluster"]["cores"]
+# account = job_properties["cluster"]["account"]
+# output = job_properties["cluster"]["output"]
+# error = job_properties["cluster"]["error"]
+
+rule = job_properties['rule']
+jobid = job_properties['jobid']
+cpus_per_task = job_properties['threads']
+log = job_properties['params']['log']
+
+outdir = config_properties['OUTDIR']
+logdir = os.path.join(outdir, "log/slurm")
+os.makedirs(logdir, exist_ok=True)
+
+resources = config_properties['RESOURCE']
+
+if resources == 'GPU' and rule in ['guppy_demultiplexing', 'guppy_demultiplexing', 'deepbinner_classification']:
+    partition = '--partition gpu --account gpu_group'
+else:
+    partition = '--partition normal --account bioinfo'
+
+
+cmdline = f'sbatch --job-name {rule} {partition} --cpus-per-task {cpus_per_task} --output {logdir}/{log}_%j --error {logdir}/{log}_%j {jobscript}'
+# sbatch --job-name {cluster.job-name} --partition {cluster.partition} --account {cluster.account} --cpus-per-task {cluster.cpus-per-task} --output {cluster.output} --error {cluster.error}
 
 os.system(cmdline)
+# print(cmdline)
