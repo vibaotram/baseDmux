c9fea3203981795dc26330cfd30be37890e56e9a
Tram <vbt576@gmail.com>
2021-03-11 21:30:43 +0700

    fast5_subset.py debug + update configfile

--- baseDmux/data/script/fast5_subset.py
+++ baseDmux/data/script/fast5_subset.py
@@ -8,30 +8,31 @@ then input each to "fast5_subset" command
 import glob
 import os
 import sys
-import multiprocessing
+from multiprocessing import Pool
 
-fast5 = sys.argv[1] # path to raw fast5 files
-path = sys.argv[2] # directory containing barcode folders
 
-summary = glob.glob(os.path.join(path, "*/sequencing_summary.txt")) # get full paths
-
-def fast5_subset(lock, summary_file, barcode_folder):
-    lock.acquire()
+def fast5_subset(summary_file):
     barcode = os.path.basename(os.path.dirname(summary_file))
-    save_path = os.path.join(barcode_folder, barcode, "fast5")
+    save_path = os.path.join(path, barcode, "fast5")
     os.makedirs(save_path, exist_ok=True)
     id_list = summary_file
     fast5_subset_cmd = "fast5_subset --input {} --save_path {} --read_id_list {} --filename_base \"{}_\"".format(fast5, save_path, id_list, barcode)
     try:
         os.system(fast5_subset_cmd)
         print("multi_reads_fast5 files created and stored in /fast5 folder for {}.".format(barcode))
+        return 1
     except ValueError:
         sys.exit("fast5_subset error at {}".format(barcode))
-    finally:
-        lock.release()
+        return 0
 
 
-for file in summary:
-    multiprocessing.Process(target=fast5_subset, args=(multiprocessing.Lock(), file, path)).start()
+if __name__ == '__main__':
+    fast5 = sys.argv[1] # path to raw fast5 files
+    path = sys.argv[2] # directory containing barcode folders
+    threads = sys.arg[3] # number of threads
 
-print(len(summary), "/fast5 folders created for corresponding barcodes.")
+    summary = glob.glob(os.path.join(path, "*/sequencing_summary.txt")) # get full paths
+
+    with Pool(threads) as p:
+        times = p.starmap(fast5_subset, summary)
+    print("{} /fast5 folders created for corresponding barcodes.". format(times))print("{} /fast5 folders created for corresponding barcodes.". format(times))
